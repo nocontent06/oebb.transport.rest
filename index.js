576ac9481d1c7cb1caa8373061cc3103e3b4ceb6
Felix <spasst.spiel@gmail.com>
2024-10-01 08:19:16 +0200

    Update using hafas-rest-api

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-import express from 'express';
-import locationsRoute from './src/req/locations.js';
-import journeysRoute from './src/req/journeys.js';
+import {createClient as createHafas} from 'hafas-client'
+import {profile as dbProfile} from 'hafas-client/p/oebb/index.js'
+import {createHafasRestApi as createApi} from 'hafas-rest-api'
+import express from 'express'
 
-const app = express();
-const port = 80;
+const config = {
+	hostname: 'v6.oebb.transport.rest',
+	name: 'OEBB Transport Rest API',
+	homepage: 'https://github.com/nocontent06/oebb.transport.rest',
+	version: '1.0.0',
+	aboutPage: false
+}
 
-// Use the locations route
-app.use('/locations', locationsRoute);
+const hafas = createHafas(dbProfile, 'admin@macistry.com')
+const api = await createApi(hafas, config)
 
-// Use the journeys route
-app.use('/journeys', journeysRoute);
+const app = express()
 
-// Start the Express server
-app.listen(port, () => {
-    console.log(`API is running on http://localhost:${port}`);
-});
+// Mount the API under the /api path
+app.use('/api', api)
+
+app.listen(443, (err) => {
+    console.log('Listening on http://localhost:443')
+    if (err) console.error(err)
+})
--- src/createHafasClient.js
+++
@@ -1,8 +0,0 @@
-
-import { createClient } from 'hafas-client';
-import { profile as oebbProfile } from 'hafas-client/p/oebb/index.js';
-
-const userAgent = 'your-hafas-project-email'; // adapt this to your project
-const client = createClient(oebbProfile, userAgent);
-
-export default client;
